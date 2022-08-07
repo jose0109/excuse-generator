179a6c4ae220b5325a49e66cba1d1bd25fe25c68
jose0109 <martinezujd@gmail.com>
2022-08-07 19:16:30 +0000

    finished code

--- src/app.js
+++ src/app.js
@@ -5,7 +5,29 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+
 window.onload = function() {
+  let randWho = who[Math.floor(Math.random() * who.length)];
+  let randAction = action[Math.floor(Math.random() * action.length)];
+  let randWhat = what[Math.floor(Math.random() * what.length)];
+  let randWhen = when[Math.floor(Math.random() * when.length)];
+
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(
+    "#excuse"
+  ).innerHTML = `${randWho} ${randAction} ${randWhat} ${randWhen}`;
+
+  document.querySelector(
+    "#excuseHeader"
+  ).innerHTML = `OMG you will not believe me but...`;
 };
